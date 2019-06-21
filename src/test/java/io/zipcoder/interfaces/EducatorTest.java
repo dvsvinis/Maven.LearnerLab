2317d3a78c7b339acb312aa1a7f140008fd83970
donnasvinis <dvsvinis@mac.com>
2019-06-20 22:19:53 -0400

    Educator Tests passed

--- src/test/java/io/zipcoder/interfaces/EducatorTest.java
+++ src/test/java/io/zipcoder/interfaces/EducatorTest.java
@@ -13,16 +13,40 @@ public class EducatorTest {
     }
 
     @Test
-    public void getInstructor() {
+    public void testImplementation() {
+        assertTrue(Educator.DOLIO instanceof Teacher);
+    }
+
+    @Test
+    public void testInheritance() {
+        assertTrue(Educator.KRIS.getInstructor() instanceof Person);
     }
 
     @Test
     public void teach() {
+        Student s3 = new Student(20, "Mickey Mouse");
+        Double expected = 6.0;
+        Educator.DOLIO.teach(s3,expected);
+        Double actual = s3.getTotalStudyTime();
+        assertEquals(expected, actual);
     }
 
     @Test
     public void lecture() {
+        Student s1 = new Student(10, "John A.");
+        Student s2 = new Student(15, "Alex B.");
+        Student s3 = new Student(20, "Mickey Mouse");
+        Learner[] students = new Student[]{s1, s2, s3};
+        Double expected = 2.0;
+        Educator.KRIS.lecture(students,expected * 3);
+        Double actual = s1.getTotalStudyTime();
+        assertEquals(expected, actual);
+        actual = s2.getTotalStudyTime();
+        assertEquals(expected, actual);
+        actual = s3.getTotalStudyTime();
+        assertEquals(expected, actual);
     }
+
 }
 
 /**
